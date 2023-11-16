b126e5e1a411656d90c78f71454dba10029ce215
safarmurodmusulmonov <safarmurodmusulmonov@gmail.com>
2023-11-16 09:22:51 +0500

    back button

---
+++ lib/widget/component/back_button_custom.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class BackButtonCustom extends StatelessWidget {
+  final double height;
+  final double width;
+
+  const BackButtonCustom({
+    super.key,
+    required this.height,
+    required this.width,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 0),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 6.0),
+        child: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+            size: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
